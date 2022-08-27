ee8c3adffb5676f84fb7e7563db142468ceb9506
DESKTOP-RVMLKBQ\Anita Kiran <anita.kiran@rainsoftfn.com>
2022-08-27 04:02:10 +0200

    initial project structure

---
+++ app/src/main/java/com/kickbase/matches/ui/view/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kickbase.matches.ui.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kickbase.matches.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
